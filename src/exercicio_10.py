913ba076db6ef8a74b8ba41dca4c79368b2306d4
Juliana Andrade <juli24013@gmail.com>
2026-03-31 23:16:18 -0300

    correção final

--- src/exercicio_10.py
+++ src/exercicio_10.py
@@ -8,4 +8,4 @@ def greet_names(names: list[str]) -> list[str]:
     Returns:
         list[str]: lista com mensagens "Hello, <name>!"
     """
-    return [f"Hello, {name}" for name in names]
+    return [f"Hello, {name}!" for name in names]
